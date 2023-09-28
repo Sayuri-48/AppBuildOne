5574633f21cef5b7508c3abca594f02a58a1a12d
Sayuri-48 <sayuri48@hawaii.edu>
2023-09-27 22:47:20 -1000

    Completed AppBuildOne

--- AppBuildOne/ViewController.swift
+++ AppBuildOne/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var item1Label: UILabel!
+    
+    @IBOutlet weak var item1Textview: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        item1Label.text =  "University of Hawaii West Oahu"
+        item1Textview.text = "UH West Oahu is the fastest growing school of its type in the nation, according to the Chronicle of Higher Education’s annual almanac.The chronicle’s Almanac of Higher Education 2018–19 listed UH West Oʻahu’s enrollment growth tops among 86 public baccalaureate granting institutions. Enrollment jumped 239 percent between 2006, when the campus had 866 students, and 2016, when there were 2,939 students."
+        
+        self.view.backgroundColor = UIColor.systemGray4
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func item1UHWOButtonPressed(_ sender: Any)
+    {
+        item1Label.text =  "University of Hawaii West Oahu"
+        item1Textview.text = "UH West Oahu is the fastest growing school of its type in the nation, according to the Chronicle of Higher Education’s annual almanac.The chronicle’s Almanac of Higher Education 2018–19 listed UH West Oʻahu’s enrollment growth tops among 86 public baccalaureate granting institutions. Enrollment jumped 239 percent between 2006, when the campus had 866 students, and 2016, when there were 2,939 students."
+    }
+    
+    
+    @IBAction func item1ACMButtonPressed(_ sender: Any)
+    {
+        item1Label.text = "About ACM"
+        item1Textview.text = "Students who chose to study creative media will receive an innovative educational experience that weaves art and design, narrative, information science, and technologies necessary for a broad range of careers in government and non-government organizations, educational institutions, technological firms and private firms and agencies."
+    }
+    
+    
 
 }
 
---
+++ AppBuildOne/secondViewController.swift
@@ -0,0 +1,47 @@
+//
+//  secondViewController.swift
+//  AppBuildOne
+//
+//  Created by binh phan on 9/27/23.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    var int1 = 39
+    var int2 = 58
+    
+    
+    @IBOutlet weak var item2Label1: UILabel!
+    
+    @IBOutlet weak var item2Label2: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        item2Label1.text = "The Answer is"
+        item2Label2.text = ""
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func item2Button(_ sender: Any)
+    {
+        let int3 = int1 + int2
+        
+        if int3 < 100
+        {
+            self.view.backgroundColor = UIColor.magenta
+            item2Label2.text = "\(int3)"
+        }
+        else
+        {
+            self.view.backgroundColor = UIColor.systemGreen
+            item2Label2.text = "\(int3)"
+        }
+        
+    }
+    
+}
